10b08acd699b5c97be7bcfc4499356bdc3ad69ee
PC-조광연 <rhkdwh91@gmail.com>
2022-01-01 16:24:57 +0900

    ADD: slite3 설치, mysql에서 sqlite 로 변경 작업1

--- pages/admin/careers.tsx
+++ pages/admin/careers.tsx
@@ -1,28 +1,25 @@
 import React from "react";
-import { getApolloClient } from 'lib/apolloClient';
+import { getApolloClient } from "lib/apolloClient";
 import { GetServerSideProps } from "next";
 import { GET_CAREERS } from "hooks/useCareersQuery";
 
-export default function Careers (props) {
+export default function Careers(props) {
   if (props?.login_check) {
     return (
       <div>
         {props?.careers?.map((data) => {
-              return (
-                  <div>
-                    <p>{data.companyName}</p>
-                    <p>{data.companyProject}</p>
-                  </div>
-              )  
-          })}
+          return (
+            <div>
+              <p>{data.companyName}</p>
+              <p>{data.companyProject}</p>
+            </div>
+          );
+        })}
       </div>
-    )
+    );
   } else {
-    <div>
-      로그인이나 하쇼
-    </div>
+    <div>로그인이나 하쇼</div>;
   }
-
 }
 
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
@@ -44,6 +41,6 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
     props: {
       login_check,
       careers: login_check ? data.careers : [],
-    }
+    },
   };
-}; 
+};
--- sqlz/index.ts
+++ sqlz/index.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize-typescript';
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/config/config.js")[env];
 
+/*
 export const sequelize = new Sequelize({
   database: config.database,
   username: config.username,
@@ -10,5 +11,14 @@ export const sequelize = new Sequelize({
   port: config.port,
   dialect: config.dialect,
   pool: config.pool,
+  models: [__dirname + '/models']
+});
+*/
+export const sequelize = new Sequelize({
+  database: config.database,
+  pool: config.pool,
+  dialect: 'sqlite',
+  //storage:'official_website.db',
+  storage:'./sqlz/config/blog.db',
   models: [__dirname + '/models']
 });
